7597faf15638bf9dd9a9aa2856ac08d72ecc3a9f
Phlares <ryon.everett@gmail.com>
2017-05-19 15:52:46 -0400

    Added PlayerCharacterScript
    Added Player Character Script to the /Utilities

---
+++ Assets/Scripts/Utility/PlayerCharacter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCharacter : MonoBehaviour {
+
+
+	//PlayerHealth
+	//PlayerAudio
+
+	string playerName;
+	GameObject playerGameObject;
+	int playerID;
+	int playerVariantNumber;
+	//PlayerHealth playerCharacterHealth
+	//PlayerAduio playerCharacterAudio
+	int victoryCount;
+	int knockoutCount;
+	Sprite playerPortrait;
+	AudioClip victoryBleat;
+	int deafaultSpronginess;
+
+
+
+
+	// Use this for initialization
+	void Awake () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void AddToVictoryCount(){
+		victoryCount++;
+	}
+
+	void AddToKnockoutCount(){
+		knockoutCount++;
+	}
+
+	void DebugPrintout(){
+		Debug.Log (playerName);
+		Debug.Log (playerGameObject);
+		Debug.Log (playerID);
+		Debug.Log (playerName);
+	}
+
+	bool TrueIfNotNull<T>(T variableToTest){
+		if (T == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
