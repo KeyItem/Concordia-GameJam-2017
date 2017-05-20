e53f60369fd0796de695280da3992f8f451a470e
Chron <ChronoblastGamesOfficial@gmail.com>
2017-05-20 12:03:09 -0400

    Added Testing Grounds

---
+++ Assets/Scripts/Systems/OutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour 
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = Vector3.zero + Vector3.up;
+        }
+    }
+}
