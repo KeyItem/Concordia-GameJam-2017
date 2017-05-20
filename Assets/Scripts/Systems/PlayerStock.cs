92b0fb636c70bff5bc687f6cdeafc77f5b797acd
Phlares <ryon.everett@gmail.com>
2017-05-20 17:01:33 -0400

    Added a PlayerStock Script
    Now with lives!

---
+++ Assets/Scripts/Systems/PlayerStock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStock : MonoBehaviour {
+
+	public int currentStock = 3;
+	public int deathStock = 0;
+	GameObject thisPlayer;
+	public bool isStillAlive;
+
+	// Use this for initialization
+	void Start () {
+		thisPlayer = this.gameObject;
+		isStillAlive = (currentStock > deathStock);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void AddStock(){
+		currentStock++;
+		Debug.Log (thisPlayer.name + " has" + currentStock + " lives left");
+	}
+	void RemoveStock(){
+		currentStock--;
+		Debug.Log (thisPlayer.name + " has" + currentStock + " lives left");
+	}
+		
+	public bool IsPlayerAlive(){
+		if (currentStock > deathStock) {
+			isStillAlive = true;
+			return true;
+		} else {
+			isStillAlive = false;
+			return false;
+		}
+	}
+
+}
