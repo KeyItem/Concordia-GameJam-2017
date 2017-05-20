9c0576fae6746318dee2f293c5c433b8cca438cb
Chron <ChronoblastGamesOfficial@gmail.com>
2017-05-20 11:46:26 -0400

    Added Llama Movement, Llama Head Control

--- Assets/Scripts/Entities/Llama/LlamaPlayerController.cs
+++ Assets/Scripts/Entities/Llama/LlamaPlayerController.cs
@@ -8,48 +8,86 @@ public class LlamaPlayerController : MonoBehaviour
     [Space(10)]
     public Rigidbody neckBody;
 
-    [Header("Llama Player Movement Values")]
+    [Header("Llama Movement Values")]
     public float baseMovementSpeed;
+    [Space(10)]
+    public float moveSmoothTime;
 
-    public Vector2 inputMovementVector;
-    public Vector2 inputHeadVector;
+    private float currentSpeed;
+    private float targetSpeed;
+    private float moveSmoothVelocity;
 
-    private Vector3 movementVector;
+    private Vector3 movingVector;
+    private Vector2 directionVector;
+
+    [Header("Llama Turning Movement Values")]
+    public float turningSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    private float targetRotation = 0f;
+
+    private Vector3 turningVector;
 
     [Header("Llama Neck Movement Values")]
     public float baseNeckControlSpeed;
 
     private Vector3 headVector;
 
+    [Header("Debug")]
+    public Vector2 inputMovementVector;
+    public Vector2 inputHeadVector;
+    [Space(10)]
+    public float currentVelocity;
+
     private void FixedUpdate()
     {
         ReadInput();
+
+        ManageBodyMovement();
+
+        ManageBodyRotation();
+
+        ManageHeadMovement();
     }
 
     private void ReadInput()
+    {       
+        directionVector = inputMovementVector.normalized;
+
+        headVector.Set(inputHeadVector.x, 0, inputHeadVector.y);
+    }
+
+    private void ManageBodyMovement()
     {
         if (inputMovementVector != Vector2.zero)
         {
-            Move();
+            targetSpeed = baseMovementSpeed * inputMovementVector.magnitude;
+
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref moveSmoothVelocity, moveSmoothTime);
+
+            movingVector = transform.forward * currentSpeed;
+
+            mainBody.AddForce(movingVector * Time.fixedDeltaTime, ForceMode.Impulse);           
         }
 
-        if (inputHeadVector != Vector2.zero)
+        currentVelocity = mainBody.velocity.magnitude;
+    }
+
+    private void ManageBodyRotation()
+    {
+        if (directionVector != Vector2.zero)
         {
-            MoveHead();
+            targetRotation = Mathf.Atan2(directionVector.x, directionVector.y) * Mathf.Rad2Deg;
+
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turningSmoothTime);
         }
     }
 
-    private void Move()
+    private void ManageHeadMovement()
     {
-        movementVector.Set(inputMovementVector.x, 0, inputMovementVector.y);
-
-        mainBody.AddForce(movementVector * baseMovementSpeed * Time.fixedDeltaTime);
-    }
-
-    private void MoveHead()
-    {
-        headVector.Set(inputHeadVector.x, 0, inputHeadVector.y);
-
-        neckBody.AddForce(headVector * baseNeckControlSpeed * Time.fixedDeltaTime);
+        if (headVector != Vector3.zero)
+        {
+            neckBody.AddForce(headVector * baseNeckControlSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
+        }
     }
 }
