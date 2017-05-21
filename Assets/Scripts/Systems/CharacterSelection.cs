81a0031a0029e7318f92b58d1842a9745deb318d
Phlares <ryon.everett@gmail.com>
2017-05-21 11:20:31 -0400

    Did more work on character selection

--- Assets/Scripts/Systems/CharacterSelection.cs
+++ Assets/Scripts/Systems/CharacterSelection.cs
@@ -6,12 +6,30 @@ using Rewired;
 
 public class CharacterSelection : MonoBehaviour {
 
+	[Space(10)]
+	[Header("Public Lists")]
+	[Space(10)]
+
     public PlayerCharacter[] characterList;
     public Horn[] hornList;
-	//player list
-	public PlayerLobbyInput[] playersInGameList; //update this list to match the number of controllers plugged in
 	public GameObject[] playerPanelList;
+	public PlayerLobbyInput[] playersInGameList; //update this list to match the number of controllers plugged in
+
+	[Space(10)]
+	[Header("To be passed to next scene with information")]
+	[Space(10)]
+	public PlayerCharacter[] playersToStartGameList;
+	public Horn[] hornToStartGameList;
+
+
+	[Space(10)]
+	[Header("UI stuff")]
+	[Space(10)]
 	public Text pressStartText;
+
+	[Space(10)]
+	[Header("Variables to Track")]
+	[Space(10)]
 	public bool readyToPlay = false;
 
 
@@ -26,7 +44,23 @@ public class CharacterSelection : MonoBehaviour {
 	}
 
 	public void SaveCharacterSelection(){
-	
+		playersToStartGameList = new PlayerCharacter[playersInGameList.Length];
+		hornToStartGameList = new Horn[playersInGameList.Length];
+
+		for (int i = 0; i < playersInGameList.Length; i++)
+		{
+			if (playersInGameList [i].isAssigned)
+			{
+				playersToStartGameList [i] = playersInGameList [i].currentCharacter;
+				hornToStartGameList [i] = playersInGameList [i].currentHorn;
+			}
+
+			else 
+			{
+				playersToStartGameList [i] = null;
+				hornToStartGameList [i] = null;
+			}
+		} 
 	}
 
     public void StartGame() {
@@ -36,35 +70,36 @@ public class CharacterSelection : MonoBehaviour {
     }
 	public void EnableStartMatch(){
 			
-			for (int i = 0; i < playersInGameList.Length; i++) {
-				//Debug.Log ("I ran the damn loop");
+		for (int i = 0; i < playersInGameList.Length; i++) {
+			//Debug.Log ("I ran the damn loop");
 
-				//If they selected a panel "isAssigned is true"
-				if (playersInGameList [i].isAssigned) {
+			//If they selected a panel "isAssigned is true"
+			if (playersInGameList [i].isAssigned) {
 
-					//If they did all of the steps (are confirmed) this is true
-					if (playersInGameList [i].hasConfirmed == true) {
-						readyToPlay = true;
-						//Debug.Log (" I got it right ");
+				//If they did all of the steps (are confirmed) this is true
+				if (playersInGameList [i].hasConfirmed == true) {
+					readyToPlay = true;
+					//Debug.Log (" I got it right ");
 
 
-					} else 
-					//They are assigned but aren't ready
-						{
-						readyToPlay = false;
-						pressStartText.enabled = false;
-						//Debug.Log (" Someone wasn't ready ");
+				} else 
+ {					//They are assigned but aren't ready
+					readyToPlay = false;
+					pressStartText.enabled = false;
+					//Debug.Log (" Someone wasn't ready ");
 
-						return;
-					} 
+					return;
 				} 
-
 			}
+//			if ((i+1) == playersInGameList.Length) {
+//				Debug.Log("Made it through the last step")
+//				readyToPlay = true;
+//			}
 			if (readyToPlay) {
 				pressStartText.enabled = true;
 			} else {
+			}
 		}
 	}
-
 
 }
